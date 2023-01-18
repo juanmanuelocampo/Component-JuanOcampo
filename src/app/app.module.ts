99ac2a513bbaff76e57927ad582e1d8caddfe6b4
Juan Ocampo <juanmanuel_ocampo@hotmail.com>
2023-01-18 19:13:37 -0300

    Clase2-entrega1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RedsocialComponent } from './components/redsocial/redsocial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MenuComponent,
+    RedsocialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/redsocial/redsocial.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redsocial',
+  templateUrl: './redsocial.component.html',
+  styleUrls: ['./redsocial.component.css']
+})
+export class RedsocialComponent {
+
+}
